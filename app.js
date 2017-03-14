26677dd378b78b791efb829e5c106e9e862a28eb
M-Abozaid <aapozaid@gmail.com>
2017-03-15 00:32:27 +0200

    fixinig wit()

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const findOrCreateSession = (fbid) => {
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
+//app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
 /*
