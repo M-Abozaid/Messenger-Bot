fdc2754cb14284849ed9228c1d0a1bcb4e8264fc
M-Abozaid <aapozaid@gmail.com>
2017-03-15 00:28:53 +0200

    fixinig wit()

--- bot.js
+++ bot.js
@@ -486,10 +486,11 @@ const actions = {
 
 };// end of actions
 
-//const accessToken = "DQF23TOEJU3ASQTI3IKPJ33HUP64A3CZ";
-const accessToken = "SXBIU2HR6Z3WKFS2OKWH56GRI7J7KINW";
+const accessToken = "DQF23TOEJU3ASQTI3IKPJ33HUP64A3CZ";
+//const accessToken = "SXBIU2HR6Z3WKFS2OKWH56GRI7J7KINW";
 const getWit = () => {
-  return new Wit(accessToken, actions);
+  return new Wit({accessToken : accessToken, 
+    actions:actions});
 };
 
 exports.getWit = getWit;
